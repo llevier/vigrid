86665449e0d5ab05030776c79d8cc99826a51efd
Laurent LEVIER <llevier@argosnet.om>
2023-10-24 14:30:29 +0000

    Vigrid API: changes to relay POST data for Vigrid-NAS polling. Now possible to filter net, disk & request dirs details

--- install/vigrid2-gns-install.sh
+++ install/vigrid2-gns-install.sh
@@ -2189,7 +2189,17 @@ server {
 
   # Vigrid API
   location /vigrid-api
-  { rewrite ^/vigrid-api/(.*)$ /manager/vigrid-api.html?order=$1 permanent; }
+  {
+    rewrite ^/vigrid-api/(.*)$ /vigrid-api.html?order=$1 break;
+    proxy_pass https://localhost;
+    proxy_pass_request_body on;
+    proxy_set_header Host $host;
+    proxy_set_header X-Real-IP $remote_addr;
+    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+    proxy_set_header X-Forwarded-Proto $scheme;
+    proxy_set_header Upgrade $http_upgrade;
+    proxy_set_header Connection \"upgrade\";
+  }
 
   # Vigrid management pages
   location /manager

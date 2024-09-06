ceb039eff6f0208b15044858110ec941b0df0b21
Laurent LEVIER <llevier@argosnet.om>
2024-09-06 07:02:18 +0000

    Vigrid GNS2 install: mistake in pattern for NGinx API configuration

--- install/vigrid2-gns-install.sh
+++ install/vigrid2-gns-install.sh
@@ -2148,8 +2148,9 @@ request_terminate_timeout = 300
     fi
 
     sed -i "s/%%PHP_VER%%/$PHP_VER/" /etc/nginx/sites/CyberRange-443-api.conf
-    sed -i "s/%%VIGRID_ROOT%%/\/home/gns3\/vigrid/" /etc/nginx/sites/CyberRange-443-api.conf
+    sed -i 's/%%VIGRID_ROOT%%/\/home\/gns3\/vigrid/' /etc/nginx/sites/CyberRange-443-api.conf
     sed -i "s/%%VIGRID_API%%/vigrid-api/" /etc/nginx/sites/CyberRange-443-api.conf
+
   fi
 
   cp /home/gns3/vigrid/confs/nginx/nginx.conf /etc/nginx/nginx.conf
